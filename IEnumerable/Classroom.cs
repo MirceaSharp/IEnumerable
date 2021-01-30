5e624bf7118d60d521eab5eb4feac462f51b066e
MirceaSharp <mircea_vasile_petrescu@yahoo.com>
2021-01-30 20:08:57 +0100

    Initial commit

---
+++ IEnumerable/Classroom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable
+{
+    class Classroom
+    {
+        public IEnumerable<string> Students
+        {
+            get; private set;
+        }
+        public Classroom(List<string> students)
+        {
+            this.Students = students;
+
+        }
+
+    }
+}
---
+++ IEnumerable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myStudents = new List<string>();
+            myStudents.Add("Mircea");
+            Classroom myClassroom = new Classroom(myStudents);
+            Console.WriteLine(myClassroom.Students.First());
+            Console.ReadLine();
+        }
+    }
+}
